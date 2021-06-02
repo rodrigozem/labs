b47c186b6fbbae1253df407ce78b11bebb5d377a
Rodrigo <digaoguitarra@gmail.com>
2021-06-02 09:37:40 -0300

    Return minutes from time

---
+++ funcoes php/get_minutes.php
@@ -0,0 +1,18 @@
+<?php
+function get_minutes($hr)
+{
+    $time = explode(":",$hr);
+    $t1 = intval($time[0]) * 60;
+    $t2 = intval($time[1]);
+    if ( strpos($hr,'-') !== false )
+        return $t1 - $t2;
+    else
+        return $t1 + $t2;
+}
+
+/*
+echo get_minutes('02:30');
+=> 150
+echo get_minutes('-00:10');
+=> -10
+*/
